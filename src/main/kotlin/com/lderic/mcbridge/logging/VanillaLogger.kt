b48946a5438a51f1facfe8f2a4e845900126d63e
Eric <lderic0415@gmail.com>
2022-05-01 16:19:20 -0700

    Implement methods

--- src/main/kotlin/com/lderic/mcbridge/logging/VanillaLogger.kt
+++ src/main/kotlin/com/lderic/mcbridge/logging/VanillaLogger.kt
@@ -6,4 +6,12 @@ internal class VanillaLogger constructor(name: String) : AbstractLogger(name) {
     override fun pattern(logType: String): String {
         return "[%tT] [${Thread.currentThread().name}/${logType}]:".format(Date())
     }
+
+    override fun getMessage(log: String): String {
+        return log.split("]: ")[1]
+    }
+
+    override fun getLogLevel(log: String): String {
+        return log.split("]: ")[0].split("/")[1]
+    }
 }
