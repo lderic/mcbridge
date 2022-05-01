669c0886e30091ef10aee116a0c3db66864517f8
Eric <lderic0415@gmail.com>
2022-04-30 17:19:47 -0700

    Delete useless sentence

--- src/main/java/com/lderic/mcbridge/MCBridge.java
+++ src/main/java/com/lderic/mcbridge/MCBridge.java
@@ -7,7 +7,6 @@ import com.lderic.mcbridge.process.MCProcess;
 import com.lderic.mcbridge.process.ProcessFactoryKt;
 
 public class MCBridge {
-    MCBridgeProperties properties = MCBridgeProperties.INSTANCE;
     public static final Logger logger = LoggerFactory.getLogger(MCBridge.class);
     public static final String VERSION = "1.0_SNAPSHOT";
     public static final String NAME = "MCBridge";
