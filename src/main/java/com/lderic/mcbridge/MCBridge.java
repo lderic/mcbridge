40d2c74d376eb05c33dda37c559348d53ba89a09
Eric <lderic0415@gmail.com>
2022-05-02 01:55:05 -0700

    Test Main

--- src/main/java/com/lderic/mcbridge/MCBridge.java
+++ src/main/java/com/lderic/mcbridge/MCBridge.java
@@ -1,10 +1,12 @@
 package com.lderic.mcbridge;
 
 import com.lderic.mcbridge.console.Console;
+import com.lderic.mcbridge.event.Events;
 import com.lderic.mcbridge.logging.Logger;
 import com.lderic.mcbridge.logging.LoggerFactory;
+import com.lderic.mcbridge.plugin.Plugins;
 import com.lderic.mcbridge.process.MCProcess;
-import com.lderic.mcbridge.process.ProcessFactoryKt;
+import com.lderic.mcbridge.process.ProcessFactory;
 
 public class MCBridge {
     public static final Logger logger = LoggerFactory.getLogger(MCBridge.class);
@@ -13,18 +15,22 @@ public class MCBridge {
     public static final String AUTHOR = "lderic";
     public static final String GITHUB = "https://github.com/lderic/mcbridge";
 
+    public static final String COMMAND_PREFIX = "!!";
+
+    private static MCProcess currentProcess;
+
     public static void main(String[] args) {
         logger.info("Starting " + NAME + " v" + VERSION);
         logger.info("Author: " + AUTHOR);
         logger.info("GitHub: " + GITHUB);
-        logger.info("");
+        logger.info("Loading plugins...");
+        Plugins.reload();
         logger.info("Starting server...");
-        final MCProcess mc = ProcessFactoryKt.launchMinecraft();
-        if (mc == null) {
+        currentProcess = ProcessFactory.launchMinecraft();
+        if (currentProcess == null) {
             logger.error("Failed to start Minecraft server!");
             return;
         }
-
         Thread reader = new Thread(() -> {
             Console console = Console.getInstance();
             String line;
@@ -33,14 +39,30 @@ public class MCBridge {
                 if (line == null) {
                     continue;
                 }
-                if (line.equalsIgnoreCase("exit")) {
-                    System.exit(0);
-                    break;
+                if (line.startsWith(COMMAND_PREFIX)) {
+                    handleCommand(line.substring(2));
+                }else  {
+                    currentProcess.writeln(line);
                 }
-                mc.writeln(line);
+
             }
         });
         reader.start();
-        mc.start();
+        currentProcess.start();
+    }
+
+    public static void handleCommand(String command) {
+        switch (command) {
+            case "exit" -> {
+                System.exit(0);
+            }
+            case "reload" -> {
+                Plugins.reload();
+            }
+        }
+    }
+
+    public static MCProcess getCurrentProcess() {
+        return currentProcess;
     }
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,26 +1,27 @@
+import com.lderic.mcbridge.text.Color;
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStringBuilder;
+import org.jline.utils.AttributedStyle;
 
 import java.io.IOException;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Terminal terminal = TerminalBuilder.builder().system(true).build();
         LineReader reader = LineReaderBuilder.builder().terminal(terminal).build();
-        new Thread(() -> {
-            try {
-                reader.printAbove("hello\n");
-                Thread.sleep(1000);
-                reader.printAbove("hel\nlo");
-                Thread.sleep(1000);
-                reader.printAbove("\n");
-                Thread.sleep(1000);
-                reader.printAbove("world");
-            }catch (Exception ignored){}
-        }).start();
-        reader.readLine("Enter your name: ");
-
+        AttributedStyle style = AttributedStyle.DEFAULT.foregroundRgb(Color.RED.getRgb()).faint();
+        AttributedStyle style2 = AttributedStyle.DEFAULT.foregroundRgb(Color.BLUE.getRgb()).blink();
+        AttributedStringBuilder builder = new AttributedStringBuilder();
+        ;
+        reader.printAbove(
+                builder.append(new AttributedString("Hello", style))
+                        .append(new AttributedString(" World", style2))
+                        .toAttributedString()
+        );
+        Thread.sleep(5000);
     }
 }
